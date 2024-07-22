334b70e1bf7e701ce5793a4c4cd99514ded103ae
a-keyter <106679998+a-keyter@users.noreply.github.com>
2024-07-22 09:53:14 +0100

    First commit

---
+++ components/LandingNav.tsx
@@ -0,0 +1,16 @@
+
+function LandingNav() {
+  return (
+    <nav className="flex justify-between w-full max-w-4xl pt-10">
+    <div title="logo container">Ambi-Learn</div>
+    <div title="nav buttons" >
+      <ul className="flex gap-x-8">
+        <li>About</li>
+        <li>Get Learning!</li>
+      </ul>
+    </div>
+  </nav>
+  )
+}
+
+export default LandingNav
