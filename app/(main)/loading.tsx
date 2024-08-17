92139c227b3daa895b867fd06e6da12a3bfaead8
a-keyter <106679998+a-keyter@users.noreply.github.com>
2024-08-17 11:27:09 +0100

    Fix loading state container size

--- app/(main)/loading.tsx
+++ app/(main)/loading.tsx
@@ -3,9 +3,9 @@ import { Skeleton } from "@/components/ui/skeleton";
 
 export default function Loading() {
   return (
-    <div className="w-full max-w-4xl flex flex-col gap-y-4">
-      <Card className="p-2"><Skeleton className="h-60 w-full" /></Card>
-      <Card className="p-2"><Skeleton className="h-60 w-full" /></Card>
+    <div className="w-full max-w-4xl flex flex-col gap-y-4 flex-1">
+      <Card className="p-2 h-full"><Skeleton className="h-full w-full" /></Card>
+      <Card className="p-2 h-full"><Skeleton className="h-full w-full" /></Card>
     </div>
   );
 }
